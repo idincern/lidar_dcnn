530701947943c5eb5e4db6516daeb958b7d1ff7c
Matthew A Klein <gmattklein@gmail.com>
2016-12-16 00:56:23 -0500

    Added loss and optimizer.

--- scripts/hyperlidar.py
+++ scripts/hyperlidar.py
@@ -31,7 +31,7 @@ with tf.name_scope('conv_layer_0'):
                      name="kernel0")
     b0 = tf.Variable(tf.zeros([1, h[1], c[1]]), name='biases0')
     # add Leaky ReLU and Batch Normalization
-    f1 = tf.nn.conv1d(f0,k0,stride=s[0],padding='VALID') + b0
+    f1 = tf.nn.relu(tf.nn.conv1d(f0,k0,stride=s[0],padding='VALID') + b0)
 
 with tf.name_scope('conv_layer_1'):
     k1 = tf.Variable(tf.truncated_normal([k[1], c[1], c[2]],
@@ -40,7 +40,7 @@ with tf.name_scope('conv_layer_1'):
                      name="kernel1")
     b1 = tf.Variable(tf.zeros([1, h[2], c[2]]), name='biases1')
     # add Leaky ReLU and Batch Normalization
-    f2 = tf.nn.conv1d(f1,k1,stride=s[1],padding='VALID') + b1
+    f2 = tf.nn.relu(tf.nn.conv1d(f1,k1,stride=s[1],padding='VALID') + b1)
 
 with tf.name_scope('conv_layer_2'):
     k2 = tf.Variable(tf.truncated_normal([k[2], c[2], c[3]],
@@ -49,7 +49,7 @@ with tf.name_scope('conv_layer_2'):
                      name="kernel2")
     b2 = tf.Variable(tf.zeros([1, h[3], c[3]]), name='biases2')
     # add Leaky ReLU and Batch Normalization
-    f3 = tf.nn.conv1d(f2,k2,stride=s[2],padding='VALID') + b2
+    f3 = tf.nn.relu(tf.nn.conv1d(f2,k2,stride=s[2],padding='VALID') + b2)
 
 with tf.name_scope('conv_layer_3'):
     k3 = tf.Variable(tf.truncated_normal([k[3], c[3], c[4]],
@@ -58,7 +58,7 @@ with tf.name_scope('conv_layer_3'):
                      name="kernel3")
     b3 = tf.Variable(tf.zeros([1, h[4], c[4]]), name='biases3')
     # add Leaky ReLU and Batch Normalization
-    f4 = tf.nn.conv1d(f3,k3,stride=s[3],padding='VALID') + b3
+    f4 = tf.nn.relu(tf.nn.conv1d(f3,k3,stride=s[3],padding='VALID') + b3)
 
 with tf.name_scope('conv_layer_4'):
     k4 = tf.Variable(tf.truncated_normal([k[4], c[4], c[5]],
@@ -67,7 +67,7 @@ with tf.name_scope('conv_layer_4'):
                      name="kernel4")
     b4 = tf.Variable(tf.zeros([1, h[5], c[5]]), name='biases4')
     # add Leaky ReLU and Batch Normalization
-    f5 = tf.nn.conv1d(f4,k4,stride=s[4],padding='VALID') + b4
+    f5 = tf.nn.relu(tf.nn.conv1d(f4,k4,stride=s[4],padding='VALID') + b4)
 
 with tf.name_scope('fc_layer_5'):
     reshape = tf.reshape(f5, [batch_size, -1])
@@ -76,7 +76,7 @@ with tf.name_scope('fc_layer_5'):
                                          math.sqrt(float(c[5]*h[5]))),
                      name='weights5')
     b5 = tf.Variable(tf.zeros([1, 1024]), name='biases5')
-    f6 = tf.matmul(reshape,w5) + b5
+    f6 = tf.nn.relu(tf.matmul(reshape,w5) + b5)
 
 with tf.name_scope('fc_layer_6'):
     w6 = tf.Variable(tf.truncated_normal([1024, 1024],
@@ -84,7 +84,7 @@ with tf.name_scope('fc_layer_6'):
                                          math.sqrt(float(1024))),
                      name='weights6')
     b6 = tf.Variable(tf.zeros([1, 1024]), name='biases6')
-    f7 = tf.matmul(f6,w6) + b6
+    f7 = tf.nn.relu(tf.matmul(f6,w6) + b6)
 
 with tf.name_scope('fc_layer_7'):
     w7 = tf.Variable(tf.truncated_normal([1024, 26],
@@ -94,13 +94,16 @@ with tf.name_scope('fc_layer_7'):
     b7 = tf.Variable(tf.zeros([1, 26]), name='biases7')
     f8 = tf.matmul(f7,w7) + b7
 
+targets = tf.placeholder(tf.float32, [batch_size, 26])
 
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(f8,targets))
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 sess = tf.Session()
 with sess.as_default():
     tf.global_variables_initializer().run()
     batch_scans = tf.random_uniform([batch_size, h[0], c[0]], maxval=50).eval()
-    output = sess.run(f8, feed_dict={f0: batch_scans})
+    batch_targets = tf.random_uniform([batch_size, 26], maxval=float(1/26)).eval()
+    output = sess.run(train_step, feed_dict={f0: batch_scans, targets: batch_targets})
     print(output)
-    print(output.shape)
     print(h)

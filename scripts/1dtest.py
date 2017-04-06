d0f2fc6cea32cbe28b224fef9611c1f0989f0305
Matthew A. Klein <mak177@case.edu>
2017-04-06 18:24:36 -0400

    Added batch_norm.

--- scripts/1dtest.py
+++ scripts/1dtest.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from ops import *
 
 # hyperparameters - general
-batch_size = 1
+batch_size = 10
 stddev = 0.2
 bias_start = 0.0
 
@@ -25,13 +25,14 @@ with tf.variable_scope('z1'):
                          tf.random_normal_initializer(stddev=stddev))
     b1 = tf.get_variable("b", [z2_h*z2_c],
                          initializer=tf.constant_initializer(bias_start))
-    a1 = tf.matmul(z0, W1) + b1
-    z1 = lrelu(a1)
+    z1 = tf.matmul(z0, W1) + b1
 
 # z2 is our first feature map layer. Its size is (batch_size, z2_h, 1, z2_c).
-# The only operation that occurs is a reshaping and a batch norm.
+# The only operation that occurs is a reshaping and a batch norm and relu.
 with tf.variable_scope('z2'):
-    z2 = tf.reshape(z1,[-1, z2_h, 1, z2_c])
+    a2 = tf.reshape(z1,[-1, z2_h, 1, z2_c])
+    bn2 = batch_norm(name='bn2')
+    z2 = tf.nn.relu(bn2(a2))
 
 # z3 is our first convolution transpose (deconvolution is bad terminology).
 z3_h = 23
@@ -44,7 +45,8 @@ with tf.variable_scope('z3'):
     a3 = tf.nn.conv2d_transpose(z2, W3,
                                 output_shape=[batch_size, z3_h, 1, z3_c],
                                 strides=[1, 2, 1, 1]) + b3
-    z3 = lrelu(a3)
+    bn3 = batch_norm(name='bn3')
+    z3 = lrelu(bn3(a3))
 
 # z4
 z4_h = 46
@@ -57,7 +59,8 @@ with tf.variable_scope('z4'):
     a4 = tf.nn.conv2d_transpose(z3, W4,
                                 output_shape=[batch_size, z4_h, 1, z4_c],
                                 strides=[1, 2, 1, 1]) + b4
-    z4 = lrelu(a4)
+    bn4 = batch_norm(name='bn4')
+    z4 = lrelu(bn4(a4))
 
 # z5
 z5_h = 91
@@ -70,7 +73,8 @@ with tf.variable_scope('z5'):
     a5 = tf.nn.conv2d_transpose(z4, W5,
                                 output_shape=[batch_size, z5_h, 1, z5_c],
                                 strides=[1, 2, 1, 1]) + b5
-    z5 = lrelu(a5)
+    bn5 = batch_norm(name='bn5')
+    z5 = lrelu(bn5(a5))
 
 # z6 - This is the result of the generator and should be the same shape as an
 # incoming batch of scans. No batch_norm on this layer.
@@ -110,7 +114,8 @@ with tf.variable_scope('z8'):
                                                                   stddev))
     b8 = tf.get_variable('b',[z8_c], initializer=tf.constant_initializer(0.0))
     a8 = tf.nn.conv2d(z7, W8, strides=[1, 2, 1, 1], padding='SAME') + b8
-    z8 = lrelu(a8)
+    bn8 = batch_norm(name='bn8')
+    z8 = lrelu(bn8(a8))
 
 # z9
 z9_h = 23
@@ -121,7 +126,8 @@ with tf.variable_scope('z9'):
                                                                   stddev))
     b9 = tf.get_variable('b',[z9_c], initializer=tf.constant_initializer(0.0))
     a9 = tf.nn.conv2d(z8, W9, strides=[1, 2, 1, 1], padding='SAME') + b9
-    z9 = lrelu(a9)
+    bn9 = batch_norm(name='bn9')
+    z9 = lrelu(bn9(a9))
 
 # z10
 z10_h = 12
@@ -133,13 +139,15 @@ with tf.variable_scope('z10'):
     b10 = tf.get_variable('b',[z10_c],
                           initializer=tf.constant_initializer(0.0))
     a10 = tf.nn.conv2d(z9, W10, strides=[1, 2, 1, 1], padding='SAME') + b10
-    z10 = lrelu(a10)
+    bn10 = batch_norm(name='bn10')
+    z10 = lrelu(bn10(a10))
 
 # z11 - reshape back up to a big linear
 with tf.variable_scope('z11'):
-    z11 = tf.reshape(z10, [batch_size, z10_h*z10_c])
+    z11 = tf.reshape(z10, [-1, z10_h*z10_c])
 
-# z12 - reduce to one dimension with linear combination
+# z12 - reduce to one dimension with linear combination (sigmoid over lrelu and
+# no batch norm)
 with tf.variable_scope('z12'):
     W12 = tf.get_variable('W', [z10_h*z10_c, 1], tf.float32,
                          tf.random_normal_initializer(stddev=stddev))
